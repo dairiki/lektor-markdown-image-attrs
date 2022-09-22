3f70adeea3ecfedf02098da0223dcdc1b73ffc92
Jeff Dairiki <dairiki@dairiki.org>
2022-09-22 13:50:21 -0700

    Put CHANGES.md back into the PyPI readme

---
+++ hatch_build.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from typing import Any
+
+from hatchling.metadata.plugin.interface import MetadataHookInterface
+
+
+class ReadmeMetadataHook(MetadataHookInterface):
+    def update(self, metadata: dict[str, Any]) -> None:
+        readme = Path(self.root, "README.md").read_text()
+        changes = Path(self.root, "CHANGES.md").read_text()
+
+        metadata["readme"] = {
+            "content-type": "text/markdown",
+            "text": "\n".join([readme, changes]),
+        }
